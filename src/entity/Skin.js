2b9529e239a20bd42b4e35a1d609e819e4647b24
Liqee431 <94087873+Liqee431@users.noreply.github.com>
2022-06-11 23:23:10 +0800

    Fix invalid skin data size
    Also changes something in skin.js

--- src/entity/Skin.js
+++ src/entity/Skin.js
@@ -15,6 +15,8 @@
 
 class Skin {
     static ACCEPTED_SKIN_SIZES = [
+        32 * 32 * 4,
+        64.046857846424 * 64.046857846424,
         64 * 32 * 4,
         64 * 64 * 4,
         128 * 128 * 4
@@ -63,8 +65,8 @@ class Skin {
             throw new Error(`Invalid skin data size ${length} bytes (allowed sizes: ${Skin.ACCEPTED_SKIN_SIZES.join(', ')})`);
         }
 
-        if(this.#capeData !== "" && this.#capeData.length !== 8192) {
-            throw new Error("Invalid cape data size " + this.#capeData.length + " bytes (must be exactly 8192 bytes)");
+        if(this.#capeData !== "" && this.#capeData.length !== 4096) {
+            throw new Error("Invalid cape data size " + this.#capeData.length + " bytes (must be exactly 4096 bytes)");
         }
 
         if(this.#geometryData !== ""){
