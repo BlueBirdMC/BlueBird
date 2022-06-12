641717718d66e4e102a0048fee9d8ac8019b6186
Liqee431 <94087873+Liqee431@users.noreply.github.com>
2022-06-12 16:30:01 +0800

    Update AppleBird.js

--- src/AppleBird.js
+++ src/AppleBird.js
@@ -1,1 +1,26 @@
+* This file is licensed under the GNU    *
+ * General Public License 3. To use or    *
+ * modify it you must accept the terms    *
+ * of the license.                        *
+ * ___________________________            *
+ * \ @author AppleBirdMC Team /            *
+\******************************************/
 
+require("./ServerInfo");
+const Path = require("path");
+const Server = require("./Server");
+
+class AppleBird {
+	constructor() {
+		let serverInstance = new Server(Path.normalize(__dirname + "/../"), NAME, VERSION);
+		serverInstance.start();
+	}
+}
+
+module.exports = AppleBird;
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Doc
