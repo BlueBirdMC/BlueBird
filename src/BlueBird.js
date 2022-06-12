7fc59c1c660f6922e5c85003006d45894d4ad279
Liqee431 <94087873+Liqee431@users.noreply.github.com>
2022-06-12 16:27:32 +0800

    AppleBird

--- src/BlueBird.js
+++ src/BlueBird.js
@@ -1,27 +1,21 @@
-/******************************************\
- *  ____  _            ____  _         _  *
- * | __ )| |_   _  ___| __ )(_)_ __ __| | *
- * |  _ \| | | | |/ _ \  _ \| | '__/ _` | *
- * | |_) | | |_| |  __/ |_) | | | | (_| | *
- * |____/|_|\__,_|\___|____/|_|_|  \__,_| *
- *                                        *
+  
  * This file is licensed under the GNU    *
  * General Public License 3. To use or    *
  * modify it you must accept the terms    *
  * of the license.                        *
  * ___________________________            *
- * \ @author BlueBirdMC Team /            *
+ * \ @author AppleBirdMC Team /            *
 \******************************************/
 
 require("./ServerInfo");
 const Path = require("path");
 const Server = require("./Server");
 
-class BlueBird {
+class AppleBird {
 	constructor() {
 		let serverInstance = new Server(Path.normalize(__dirname + "/../"), NAME, VERSION);
 		serverInstance.start();
 	}
 }
 
-module.exports = BlueBird;
+module.exports = AppleBird;
